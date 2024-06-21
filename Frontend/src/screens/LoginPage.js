3953f9fc2406dcc6e15d185bb04d056a42491f3d
Kathan Patel <kathanpatel403@gmail.com>
2024-06-21 16:12:59 +0530

    Authentication Pages Created!

--- Frontend/src/screens/LoginPage.js
+++ Frontend/src/screens/LoginPage.js
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
-
+import axios from "axios";
 import { Icon } from 'react-icons-kit';
 import { eyeOff } from 'react-icons-kit/feather/eyeOff';
 import { eye } from 'react-icons-kit/feather/eye';
-import axios from "axios";
+import first from '../assets/images/first.jpg';
+import second from '../assets/images/second.jpg';
+import third from '../assets/images/third.jpg';
+import fourth from '../assets/images/fourth.jpg';
+
+const images = [first, second, third, fourth];
 
 function LoginPage() {
   const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -29,72 +42,82 @@ function LoginPage() {
     e.preventDefault();
 
     try {
-        const response = await axios.post('http://localhost:8000/auth/api/login/', {
-            username,
-            password
-        });
+      const response = await axios.post('http://localhost:8000/auth/api/login/', {
+        username,
+        password
+      });
 
-        console.log('Login successful:', response.data);
-        // Optionally handle success message or redirect to login page
+      console.log('Login successful:', response.data);
+      // Optionally handle success message or redirect to login page
     } catch (error) {
-        console.error('Login error:', error);
-        // Optionally handle error message (e.g., display error to user)
+      console.error('Login error:', error);
+      // Optionally handle error message (e.g., display error to user)
     }
-};
+  };
 
   return (
     <div className="login-container h-screen flex justify-center items-center bg-gray-900">
-      <div className="glass-login rounded-2xl backdrop-filter backdrop-blur-md shadow-md bg-gray-800 p-10 w-96">
-        <div className="form-container">
-          <h1 className="login-heading text-2xl font-bold text-white mb-5">Login</h1>
-
-          <label htmlFor="email" className="block text-sm font-medium text-gray-300 mb-1">Email</label>
-          <input
-            id="email"
-            type="email"
-            value={username}
-            onChange={handleUsernameChange}
-            placeholder="Enter email address"
-            className="input-field w-full p-3 mb-4 border-0 rounded-md bg-gray-700 text-gray-300 focus:outline-none focus:ring-green-500 focus:border-green-500"
+      <div className="flex justify-between items-center w-full max-w-5xl gap-5">
+        <div className="image-carousel w-1/2 h-full p-1">
+          <img
+            src={images[currentImageIndex]}
+            alt="carousel"
+            className="w-full h-full object-cover rounded-2xl"
+            style={{ width: '100%', height: '400px' }}
           />
+        </div>
+        <div className="glass-login w-1/2 rounded-2xl backdrop-filter backdrop-blur-md shadow-md bg-gray-800 p-7">
+          <div className="form-container">
+            <h1 className="login-heading text-2xl font-bold text-white mb-5">Login</h1>
 
-          <label htmlFor="password" className="block text-sm font-medium text-gray-300 mb-1">Password</label>
-          <div className="relative">
+            <label htmlFor="username" className="block text-sm font-medium text-gray-300 mb-1">Username</label>
             <input
-              id="password"
-              type={showPassword ? "text" : "password"}
-              value={password}
-              onChange={handlePasswordChange}
-              placeholder="Enter Password"
+              id="email"
+              type="email"
+              value={username}
+              onChange={handleUsernameChange}
+              placeholder="Enter username"
               className="input-field w-full p-3 mb-4 border-0 rounded-md bg-gray-700 text-gray-300 focus:outline-none focus:ring-green-500 focus:border-green-500"
             />
+
+            <label htmlFor="password" className="block text-sm font-medium text-gray-300 mb-1">Password</label>
+            <div className="relative">
+              <input
+                id="password"
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={handlePasswordChange}
+                placeholder="Enter Password"
+                className="input-field w-full p-3 mb-4 border-0 rounded-md bg-gray-700 text-gray-300 focus:outline-none focus:ring-green-500 focus:border-green-500"
+              />
+              <button
+                onClick={handleTogglePasswordVisibility}
+                className="absolute right-3 top-1/3 transform -translate-y-1/2 text-gray-400"
+              >
+                <Icon icon={showPassword ? eye : eyeOff} size={20} />
+              </button>
+            </div>
+
             <button
-              onClick={handleTogglePasswordVisibility}
-              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+              onClick={handleSignIn}
+              className="login-button w-full p-3 bg-green-600 text-white rounded-md cursor-pointer hover:bg-green-700"
             >
-              <Icon icon={showPassword ? eye : eyeOff} size={20} />
+              Login
             </button>
+
+            <div className="forgot-password text-right mt-3">
+              <Link to="/forgotpassword" className="forgot-password-link text-gray-300 hover:text-green-500">
+                Forgot Password?
+              </Link>
+            </div>
+
+            <p className="signup-text text-right mt-3 text-gray-300">
+              Don't have an account?{" "}
+              <span className="signup-link">
+                <Link to="/signup" className="text-green-500 hover:underline">Sign up</Link>
+              </span>
+            </p>
           </div>
-
-          <button
-            onClick={handleSignIn}
-            className="login-button w-full p-3 bg-green-600 text-white rounded-md cursor-pointer hover:bg-green-700"
-          >
-            Login
-          </button>
-
-          <div className="forgot-password text-right mt-3">
-            <Link to="/forgotpassword" className="forgot-password-link text-gray-300 hover:text-green-500">
-              Forgot Password?
-            </Link>
-          </div>
-
-          <p className="signup-text text-right mt-3 text-gray-300">
-            Don't have an account?{" "}
-            <span className="signup-link">
-              <Link to="/signup" className="text-green-500 hover:underline">Sign up</Link>
-            </span>
-          </p>
         </div>
       </div>
     </div>
--- Frontend/src/screens/SignupPage.js
+++ Frontend/src/screens/SignupPage.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-
+import axios from 'axios';
 import { Icon } from 'react-icons-kit';
 import { eyeOff } from 'react-icons-kit/feather/eyeOff';
 import { eye } from 'react-icons-kit/feather/eye';
-import axios from 'axios';
+import first from '../assets/images/first.jpg';
+import second from '../assets/images/second.jpg';
+import third from '../assets/images/third.jpg';
+import fourth from '../assets/images/fourth.jpg';
 
+const images = [first, second, third, fourth];
 
 function SignupPage() {
     const navigate = useNavigate();
@@ -14,7 +17,16 @@ function SignupPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(false);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
     const [errorMsg, setErrorMsg] = useState("");
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, []);
 
     const handleUsernameChange = (event) => {
         setUsername(event.target.value);
@@ -50,70 +62,79 @@ function SignupPage() {
         }
     };
 
-
     return (
         <div className="signup-container h-screen flex justify-center items-center bg-gray-900">
-            <div className="glass-signup rounded-2xl backdrop-filter backdrop-blur-md shadow-md bg-gray-800 p-10 w-96">
-                <div className="form-container">
-                    <h1 className="signup-heading text-2xl font-bold text-white mb-5">Sign Up</h1>
+            <div className="flex justify-between items-center w-full max-w-5xl gap-2.5">
+                <div className="glass-signup w-1/2 rounded-2xl backdrop-filter backdrop-blur-md shadow-md bg-gray-800 p-7">
+                    <div className="form-container">
+                        <h1 className="signup-heading text-2xl font-bold text-white mb-5">Sign Up</h1>
 
-                    <label htmlFor="username" className="block text-sm font-medium text-gray-300 mb-1">Username</label>
-                    <input
-                        id="username"
-                        type="text"
-                        value={username}
-                        onChange={handleUsernameChange}
-                        placeholder="Enter username"
-                        className="input-field w-full p-3 mb-4 border-0 rounded-md bg-gray-700 text-gray-300 focus:outline-none focus:ring-green-500 focus:border-green-500"
-                    />
-
-                    <label htmlFor="email" className="block text-sm font-medium text-gray-300 mb-1">Email</label>
-                    <input
-                        id="email"
-                        type="email"
-                        value={email}
-                        onChange={handleEmailChange}
-                        placeholder="Enter email address"
-                        className="input-field w-full p-3 mb-4 border-0 rounded-md bg-gray-700 text-gray-300 focus:outline-none focus:ring-green-500 focus:border-green-500"
-                    />
-
-                    <label htmlFor="password" className="block text-sm font-medium text-gray-300 mb-1">Password</label>
-                    <div className="relative">
+                        <label htmlFor="username" className="block text-sm font-medium text-gray-300 mb-1">Username</label>
                         <input
-                            id="password"
-                            type={showPassword ? "text" : "password"}
-                            value={password}
-                            onChange={handlePasswordChange}
-                            placeholder="Enter Password"
+                            id="username"
+                            type="text"
+                            value={username}
+                            onChange={handleUsernameChange}
+                            placeholder="Enter username"
                             className="input-field w-full p-3 mb-4 border-0 rounded-md bg-gray-700 text-gray-300 focus:outline-none focus:ring-green-500 focus:border-green-500"
                         />
-                        <button
-                            onClick={handleTogglePasswordVisibility}
-                            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"
-                        >
-                            <Icon icon={showPassword ? eye : eyeOff} size={20} />
-                        </button>
-                    </div>
 
-                    {errorMsg && (
-                        <div className="error-message mb-5">
-                            <b className="text-red-500 block mb-2">{errorMsg}</b>
+                        <label htmlFor="email" className="block text-sm font-medium text-gray-300 mb-1">Email</label>
+                        <input
+                            id="email"
+                            type="email"
+                            value={email}
+                            onChange={handleEmailChange}
+                            placeholder="Enter email address"
+                            className="input-field w-full p-3 mb-4 border-0 rounded-md bg-gray-700 text-gray-300 focus:outline-none focus:ring-green-500 focus:border-green-500"
+                        />
+
+                        <label htmlFor="password" className="block text-sm font-medium text-gray-300 mb-1">Password</label>
+                        <div className="relative">
+                            <input
+                                id="password"
+                                type={showPassword ? "text" : "password"}
+                                value={password}
+                                onChange={handlePasswordChange}
+                                placeholder="Enter Password"
+                                className="input-field w-full p-3 mb-4 border-0 rounded-md bg-gray-700 text-gray-300 focus:outline-none focus:ring-green-500 focus:border-green-500"
+                            />
+                            <button
+                                onClick={handleTogglePasswordVisibility}
+                                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+                            >
+                                <Icon icon={showPassword ? eye : eyeOff} size={20} />
+                            </button>
                         </div>
-                    )}
 
-                    <button
-                        onClick={handleSignUp}
-                        className="signup-button w-full p-3 bg-green-600 text-white rounded-md cursor-pointer hover:bg-green-700"
-                    >
-                        Sign Up
-                    </button>
+                        {errorMsg && (
+                            <div className="error-message mb-5">
+                                <b className="text-red-500 block mb-2">{errorMsg}</b>
+                            </div>
+                        )}
 
-                    <div className="login-link text-center mt-3 text-gray-300">
-                        Already have an account?{" "}
-                        <span className="signup-link">
-                            <Link to="/login" className="text-green-500 hover:underline">Log in</Link>
-                        </span>
+                        <button
+                            onClick={handleSignUp}
+                            className="signup-button w-full p-3 bg-green-600 text-white rounded-md cursor-pointer hover:bg-green-700"
+                        >
+                            Sign Up
+                        </button>
+
+                        <div className="login-link text-center mt-3 text-gray-300">
+                            Already have an account?{" "}
+                            <span className="signup-link">
+                                <Link to="/login" className="text-green-500 hover:underline">Log in</Link>
+                            </span>
+                        </div>
                     </div>
+                </div>
+                <div className="image-carousel w-1/2 h-full p-1">
+                    <img
+                        src={images[currentImageIndex]}
+                        alt="carousel"
+                        className="w-full h-full object-cover rounded-2xl"
+                        style={{ width: '100%', height: '400px' }}
+                    />
                 </div>
             </div>
         </div>
