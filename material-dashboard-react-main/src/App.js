08e8a5e037c8de61a60b0d49719a33fd48dc795b
Vihar4104 <tvjcoder414@gmail.com>
2024-07-15 14:05:45 +0530

    First Commit Of Website code

---
+++ material-dashboard-react-main/src/App.js
@@ -0,0 +1,286 @@
+// /**
+// =========================================================
+// * Creative Tim React - v2.2.0
+// =========================================================
+
+// * Product Page: https://www.creative-tim.com/product/material-dashboard-react
+// * Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+// Coded by www.creative-tim.com
+
+//  =========================================================
+
+// * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// */
+
+// import { useState, useEffect, useMemo } from "react";
+
+// // react-router components
+// import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+
+// // @mui material components
+// import { ThemeProvider } from "@mui/material/styles";
+// import CssBaseline from "@mui/material/CssBaseline";
+// import Icon from "@mui/material/Icon";
+
+// // Creative Tim React components
+// import MDBox from "components/MDBox";
+
+// // Creative Tim React example components
+// import Sidenav from "examples/Sidenav";
+// import Configurator from "examples/Configurator";
+
+// // Creative Tim React themes
+// import theme from "assets/theme";
+
+
+// // Creative Tim React Dark Mode themes
+// import themeDark from "assets/theme-dark";
+
+
+// // Creative Tim React routes
+// import routes from "routes";
+
+// // Creative Tim React contexts
+// import { useMaterialUIController, setMiniSidenav, setOpenConfigurator } from "context";
+
+// // Images
+// import brandWhite from "assets/images/logo-ct.png";
+// import brandDark from "assets/images/logo-ct-dark.png";
+
+// export default function App() {
+//   const [controller, dispatch] = useMaterialUIController();
+//   const {
+//     miniSidenav,
+//     direction,
+//     layout,
+//     openConfigurator,
+//     sidenavColor,
+//     transparentSidenav,
+//     whiteSidenav,
+//     darkMode,
+//   } = controller;
+//   const [onMouseEnter, setOnMouseEnter] = useState(false);
+//   const [rtlCache, setRtlCache] = useState(null);
+//   const { pathname } = useLocation();
+
+
+
+//   // Open sidenav when mouse enter on mini sidenav
+//   const handleOnMouseEnter = () => {
+//     if (miniSidenav && !onMouseEnter) {
+//       setMiniSidenav(dispatch, false);
+//       setOnMouseEnter(true);
+//     }
+//   };
+
+//   // Close sidenav when mouse leave mini sidenav
+//   const handleOnMouseLeave = () => {
+//     if (onMouseEnter) {
+//       setMiniSidenav(dispatch, true);
+//       setOnMouseEnter(false);
+//     }
+//   };
+
+//   // Change the openConfigurator state
+//   const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
+
+//   // Setting the dir attribute for the body element
+//   useEffect(() => {
+//     document.body.setAttribute("dir", direction);
+//   }, [direction]);
+
+//   // Setting page scroll to 0 when changing the route
+//   useEffect(() => {
+//     document.documentElement.scrollTop = 0;
+//     document.scrollingElement.scrollTop = 0;
+//   }, [pathname]);
+
+//   const getRoutes = (allRoutes) =>
+//     allRoutes.map((route) => {
+//       if (route.collapse) {
+//         return getRoutes(route.collapse);
+//       }
+
+//       if (route.route) {
+//         return <Route exact path={route.route} element={route.component} key={route.key} />;
+//       }
+
+//       return null;
+//     });
+
+//   const configsButton = (
+//     <MDBox
+//       display="flex"
+//       justifyContent="center"
+//       alignItems="center"
+//       width="3.25rem"
+//       height="3.25rem"
+//       bgColor="white"
+//       shadow="sm"
+//       borderRadius="50%"
+//       position="fixed"
+//       right="2rem"
+//       bottom="2rem"
+//       zIndex={99}
+//       color="dark"
+//       sx={{ cursor: "pointer" }}
+//       onClick={handleConfiguratorOpen}
+//     >
+//       <Icon fontSize="small" color="inherit">
+//         settings
+//       </Icon>
+//     </MDBox>
+//   );
+
+//   return(
+//     <ThemeProvider theme={darkMode ? themeDark : theme}>
+//       <CssBaseline />
+//       {layout === "dashboard" && (
+//         <>
+//           <Sidenav
+//             color={sidenavColor}
+//             brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
+//             brandName="Creative Tim"
+//             routes={routes}
+//             onMouseEnter={handleOnMouseEnter}
+//             onMouseLeave={handleOnMouseLeave}
+//           />
+//           <Configurator />
+//           {configsButton}
+//         </>
+//       )}
+//       {layout === "vr" && <Configurator />}
+//       <Routes>
+//         {getRoutes(routes)}
+//         <Route path="*" element={<Navigate to="/dashboard" />} />
+//       </Routes>
+//     </ThemeProvider>
+//   );
+// }
+
+
+
+
+
+import { useState, useEffect, useMemo } from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Icon from "@mui/material/Icon";
+import MDBox from "components/MDBox";
+import Sidenav from "examples/Sidenav";
+import Configurator from "examples/Configurator";
+import theme from "assets/theme";
+import themeDark from "assets/theme-dark";
+import routes from "routes";
+import { useMaterialUIController, setMiniSidenav, setOpenConfigurator } from "context";
+import brandWhite from "assets/images/logo-ct.png";
+import brandDark from "assets/images/logo-ct-dark.png";
+
+export default function App() {
+  const [controller, dispatch] = useMaterialUIController();
+  const {
+    miniSidenav,
+    direction,
+    layout,
+    openConfigurator,
+    sidenavColor,
+    transparentSidenav,
+    whiteSidenav,
+    darkMode,
+  } = controller;
+  const [onMouseEnter, setOnMouseEnter] = useState(false);
+  const [rtlCache, setRtlCache] = useState(null);
+  const { pathname } = useLocation();
+
+  // Define routes where the Sidenav should not be displayed
+  const noSidenavRoutes = ["/login","/sign-up", "/register", "/forgot-password"];
+
+  const handleOnMouseEnter = () => {
+    if (miniSidenav && !onMouseEnter) {
+      setMiniSidenav(dispatch, false);
+      setOnMouseEnter(true);
+    }
+  };
+
+  const handleOnMouseLeave = () => {
+    if (onMouseEnter) {
+      setMiniSidenav(dispatch, true);
+      setOnMouseEnter(false);
+    }
+  };
+
+  const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
+
+  useEffect(() => {
+    document.body.setAttribute("dir", direction);
+  }, [direction]);
+
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+  }, [pathname]);
+
+  const getRoutes = (allRoutes) =>
+    allRoutes.map((route) => {
+      if (route.collapse) {
+        return getRoutes(route.collapse);
+      }
+
+      if (route.route) {
+        return <Route exact path={route.route} element={route.component} key={route.key} />;
+      }
+
+      return null;
+    });
+
+  const configsButton = (
+    <MDBox
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      width="3.25rem"
+      height="3.25rem"
+      bgColor="white"
+      shadow="sm"
+      borderRadius="50%"
+      position="fixed"
+      right="2rem"
+      bottom="2rem"
+      zIndex={99}
+      color="dark"
+      sx={{ cursor: "pointer" }}
+      onClick={handleConfiguratorOpen}
+    >
+      <Icon fontSize="small" color="inherit">
+        settings
+      </Icon>
+    </MDBox>
+  );
+
+  return (
+    <ThemeProvider theme={darkMode ? themeDark : theme}>
+      <CssBaseline />
+      {layout === "dashboard" && !noSidenavRoutes.includes(pathname) && (
+        <>
+          <Sidenav
+            color={sidenavColor}
+            brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
+            brandName="Creative Tim"
+            routes={routes}
+            onMouseEnter={handleOnMouseEnter}
+            onMouseLeave={handleOnMouseLeave}
+          />
+          <Configurator />
+          {configsButton}
+        </>
+      )}
+      {layout === "vr" && <Configurator />}
+      <Routes>
+        {getRoutes(routes)}
+        <Route path="*" element={<Navigate to="/dashboard" />} />
+      </Routes>
+    </ThemeProvider>
+  );
+}
---
+++ material-dashboard-react-main/src/assets/theme-dark/functions/rgba.js
@@ -0,0 +1,28 @@
+/**
+=========================================================
+* Creative Tim React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+/**
+  The rgba() function helps you to create a rgba color code, it uses the hexToRgb() function
+  to convert the hex code into rgb for using it inside the rgba color format.
+ */
+
+// Creative Tim React helper functions
+import hexToRgb from "assets/theme-dark/functions/hexToRgb";
+
+function rgba(color, opacity) {
+  return `rgba(${hexToRgb(color)}, ${opacity})`;
+}
+
+export default rgba;
